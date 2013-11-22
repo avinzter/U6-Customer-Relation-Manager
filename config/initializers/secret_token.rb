9c49e7125e0d3146d051dbdd445b189959dc5fc4
Geniva B. Lacara <geniva.lacara@gmail.com>
2013-11-22 08:23:13 -0600

    First commit to U6 crm challenge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsChallengeCrm::Application.config.secret_token = '688c04dafaa9a7cc48375be9b84ede8f3cc09ef228791a5add4ee28e2e4674bf70ddc68ac3e8dd83fb18e5013f6e23d759a12fd9eef6a1457e4f597cec8374c8'
